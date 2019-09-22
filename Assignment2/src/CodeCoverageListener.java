0cf0ff0b3fbbb20090b5906253af030f51805c9e
Brendan Ngo <bngo@Brendans-MBP.lan1>
2019-09-22 12:04:08 -0500

    change code coverage output to publisher

--- Assignment2/src/CodeCoverageListener.java
+++ Assignment2/src/CodeCoverageListener.java
@@ -1,16 +1,16 @@
 import gov.nasa.jpf.Config;
 import gov.nasa.jpf.JPF;
 import gov.nasa.jpf.ListenerAdapter;
+import gov.nasa.jpf.report.ConsolePublisher;
+import gov.nasa.jpf.report.Publisher;
+import gov.nasa.jpf.report.PublisherExtension;
 import gov.nasa.jpf.vm.*;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public class CodeCoverageListener extends ListenerAdapter {
+public class CodeCoverageListener extends ListenerAdapter implements PublisherExtension {
 
-    Map<String, Map<String, Set<Integer>>> classLineCoverage;
+    Map<String, Set<Integer>> classLineCoverage;
     Set<String> packages;
 
     public CodeCoverageListener() {
@@ -20,6 +20,9 @@ public class CodeCoverageListener extends ListenerAdapter {
 
     public CodeCoverageListener(Config config, JPF jpf) {
         //jpf.addPublisherExtension();
+        classLineCoverage = new HashMap<>();
+        packages = new HashSet<>();
+        jpf.addPublisherExtension(ConsolePublisher.class, this);
     }
 
     // java.*, gov.*, sun.*, java.*
@@ -32,28 +35,29 @@ public class CodeCoverageListener extends ListenerAdapter {
         String className = ci.getName();
         String packageName = ci.getPackageName();
         packages.add(ci.getPackageName());
-        if(packageName.equals("")) {
+        if(filterPackageName(packageName)) {
             int lineNumber = instructionToExecute.getLineNumber();
-            Map<String, Set<Integer>> methods = classLineCoverage.getOrDefault(className, new HashMap<String, Set<Integer>>());
-            Set<Integer> lineNumbers = methods.getOrDefault(methodName, new HashSet<Integer>());
+            Set<Integer> lineNumbers = classLineCoverage.getOrDefault(className, new TreeSet<Integer>());
             lineNumbers.add(lineNumber);
-            methods.put(methodName, lineNumbers);
-            classLineCoverage.put(className, methods);
+            classLineCoverage.put(className, lineNumbers);
         }
     }
 
-    public void classLoaded(VM vm, ClassInfo ci) {
-        
+    private boolean filterPackageName(String packageName) {
+        return !packageName.startsWith("java.") &&
+                !packageName.startsWith("sun.") &&
+                !packageName.startsWith("gov.nasa.jpf");
     }
 
     @Override
-    public void gcEnd(VM vm) {
-        System.out.println(classLineCoverage);
-//        System.out.println(packages);
-        // OUTPUT TO FILE
+    public void publishFinished (Publisher publisher) {
+        writeCoverageResults();
     }
 
-
+    private void writeCoverageResults() {
+        System.out.println(packages);
+        System.out.println(classLineCoverage);
+    }
 
 }
 
