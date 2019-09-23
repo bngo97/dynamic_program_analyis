47c0be04b9373b26e73b18686b3870bcc7916842
Brendan Ngo <bngo@wireless-10-147-131-212.public.utexas.edu>
2019-09-22 23:48:08 -0500

    fixed runner script

--- Assignment2/run_examples.sh
+++ Assignment2/run_examples.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 git clone https://github.com/javapathfinder/jpf-core.git
-cp src/*.java jpf-core/src/
-./gradelw
+cp src/*.java jpf-core/src/examples/
+cd jpf-core
+./gradlew
+cd ..
 jpf-core/bin/jpf +classpath=jpf-core/ +listener=CodeCoverageListener,MemoizationListener ExampleRunner
