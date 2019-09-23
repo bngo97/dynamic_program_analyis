aeb45efecd459b9772fd14a791e7426000dc0cd7
Brendan Ngo <bngo@wireless-10-147-131-212.public.utexas.edu>
2019-09-23 00:08:18 -0500

    fixed script for real

--- Assignment2/run_examples.sh
+++ Assignment2/run_examples.sh
@@ -2,7 +2,8 @@
 
 git clone https://github.com/javapathfinder/jpf-core.git
 cp src/*.java jpf-core/src/examples/
+cp src/*Listener.java jpf-core/src/main/gov/nasa/jpf/listener/
 cd jpf-core
 ./gradlew
-cd ..
-jpf-core/bin/jpf +classpath=jpf-core/ +listener=CodeCoverageListener,MemoizationListener ExampleRunner
+bin/jpf +classpath=. +listener=CodeCoverageListener,MemoizationListener ExampleRunner
+mv coverage.txt ../
