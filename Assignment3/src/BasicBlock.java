4e0e5e66870e4a348901b116fa7e24503d5a418c
Brendan Ngo <bngo@wireless-10-145-128-26.public.utexas.edu>
2019-10-14 23:01:59 -0500

    remove unused code in basic block

--- Assignment3/src/BasicBlock.java
+++ Assignment3/src/BasicBlock.java
@@ -9,19 +9,17 @@ public class BasicBlock {
 
     int blockId;
     Label label;
-    List<Label> labels;
     Set<BasicBlock> connections;
     boolean jumpedTo;
 
     public BasicBlock(Label l) {
         blockId = -1;
-        labels = new ArrayList<>();
         connections = new HashSet<>();
         this.label = l;
     }
 
     @Override
     public String toString() {
-        return "BB"+blockId;
+        return "BB" + blockId;
     }
 }
